3552c74e681bd5714b53319928ec11e54002c307
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-16 14:48:32 -0500

    Adjusted API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://hidden-plateau-58997.herokuapp.com/',
+  API_ENDPOINT: 'https://hidden-plateau-58997.herokuapp.com',
   TOKEN_KEY: '960f52b8-cd60-4fdb-8d93-30cdd2b3c32f',
 }

2d28401ecdf8b84911bb196c238e2d4560b64694
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-16 14:26:02 -0500

    Adjusted API_ENDPOINT and TOKEN_KEY

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: 'https://hidden-plateau-58997.herokuapp.com/',
+  TOKEN_KEY: '960f52b8-cd60-4fdb-8d93-30cdd2b3c32f',
 }
